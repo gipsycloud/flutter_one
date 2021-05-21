1161eef2ae77701663e23f3c66663fc58d013c2c
aungwin <aungwinygn1990@gmail.com>
2021-05-21 17:19:20 +0630

    adding register page

--- lib/view/page/register_page/register_page.dart
+++ lib/view/page/register_page/register_page.dart
@@ -1,27 +1,65 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_neumorphic/flutter_neumorphic.dart';
-import 'package:help/view/page/register_page/register_page.dart';
 
-class LoginPage extends StatefulWidget {
+class RegisterPage extends StatefulWidget {
   @override
-  _LoginPageState createState() => _LoginPageState();
+  _RegisterPageState createState() => _RegisterPageState();
 }
 
-class _LoginPageState extends State<LoginPage> {
+class _RegisterPageState extends State<RegisterPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey.shade100,
+      appBar: NeumorphicAppBar(
+        title: Text("Register"),
+      ),
       body: SingleChildScrollView(
         padding: EdgeInsets.all(16),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
             SizedBox(
-              height: 100,
+              height: 20,
             ),
-            FlutterLogo(
-              size: 100,
+            Neumorphic(
+              style: NeumorphicStyle(
+                shape: NeumorphicShape.flat,
+                boxShape: NeumorphicBoxShape.circle(),
+                depth: -4,
+                lightSource: LightSource.topLeft,
+              ),
+              child: Container(
+                width: 100,
+                height: 100,
+                child: Column(
+                  children: [
+                    Icon(Icons.person),
+                    Text(
+                      "Upload\nPhoto",
+                      textAlign: TextAlign.center,
+                    )
+                  ],
+                ),
+              ),
+            ),
+            Neumorphic(
+              style: NeumorphicStyle(
+                shape: NeumorphicShape.concave,
+                boxShape:
+                    NeumorphicBoxShape.roundRect(BorderRadius.circular(12)),
+                depth: -4,
+                lightSource: LightSource.topLeft,
+              ),
+              child: TextField(
+                decoration: InputDecoration(
+                    hintText: "Enter Name",
+                    border: InputBorder.none,
+                    contentPadding: EdgeInsets.only(left: 20)),
+              ),
+            ),
+            SizedBox(
+              height: 20,
             ),
             Neumorphic(
               style: NeumorphicStyle(
@@ -57,39 +95,9 @@ class _LoginPageState extends State<LoginPage> {
               ),
             ),
             SizedBox(
-              height: 30,
-            ),
-            NeumorphicButton(
-              onPressed: () {
-                Navigator.of(context)
-                    .push(MaterialPageRoute(builder: (context) {
-                  return RegisterPage();
-                }));
-              },
-              child: Center(
-                child: Text(
-                  "Login",
-                  style: TextStyle(fontWeight: FontWeight.bold),
-                ),
-              ),
-              style: NeumorphicStyle(
-                shape: NeumorphicShape.flat,
-                boxShape:
-                    NeumorphicBoxShape.roundRect(BorderRadius.circular(12)),
-                depth: 8,
-                lightSource: LightSource.topLeft,
-              ),
-            ),
-            SizedBox(
               height: 20,
             ),
             NeumorphicButton(
-              onPressed: () {
-                Navigator.of(context)
-                    .push(MaterialPageRoute(builder: (context) {
-                  return RegisterPage();
-                }));
-              },
               child: Center(
                 child: Text(
                   "Register",
